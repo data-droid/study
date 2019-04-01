b079043459c9ca5746b1f734c281a35a38da9c18
data-droid <blue44rain@gmail.com>
2019-04-01 23:48:05 +0900

    Chapter 4~6

--- kaggleLearn/python2.py
+++ kaggleLearn/python2.py
@@ -68,7 +68,127 @@ def quiz_message(grade) :
 quiz_message(80)
 
 def quiz_message(grade) :
-	outcome = 'failed' if grade < 50 else 'passed'
+	outcome = 'failed' if grade < 50 else 'passed' # 특이함!
 	print('You', outcome, 'the quiz with a grade of',grade)
 
 quiz_message(40)
+
+
+#Chapter 4 : LIST
+primes = [2,3,5,7]
+print(primes)
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets)
+hands = [['J', 'Q', 'K'], ['2', '2', '2'], ['6', 'A', 'K']] # list of list
+print(hands)
+my_favorite_things = [32,'raindrops on roses',help] #mixed type
+print(my_favorite_things)
+
+print(planets[1]) #indexing from 0 >>Venus
+print(planets[-1]) #indexing Negative from back >> Neptune
+
+print(planets[1:3]) #Slicing 1<=i<3
+print(planets[:3]) #Slicing i<3
+print(planets[3:]) #Slicing i>=3
+
+print(planets[1:-1]) #Slicing 1<=i<last
+print(planets[-3:]) #Slicing last-3<=i
+
+planets[3] = "Malacandra" 
+print(planets)
+
+planets[:3] = ['Mur', 'Vee', 'Ur']
+print(planets)
+
+planets[:4] = ['Mercury', 'Venus', 'Earth', 'Mars']
+print(planets)
+
+print(len(planets)) #List length
+print(sorted(planets)) #dic sort
+
+print(primes, sum(primes)) # list sum
+print(primes, max(primes)) # list max
+
+##
+x = 12
+print(x.imag) # x is a real number, imaginary part is 0.
+c = 12 + 3j
+print(c.imag) # 3.0
+print(x.bit_length()) # bit변환시 길이
+##
+
+planets.append('Pluto') # append
+print(planets)
+
+print(planets.pop()) # pop
+print(planets)
+print(planets.index('Earth')) # search 
+#print(planets.index('Pluto')) # error!
+print("Earth" in planets) #True
+print("Calefraques" in planets) #False
+#help(planets) 
+
+#Tuples
+t = (1,2,3)
+print(t)
+t = 1, 2, 3
+print(t)
+# t[0] = 100 # error! cannot be modified
+x = 0.125
+print(x.as_integer_ratio()) # 분자/분모 값 반환 
+numerator, denominator = x.as_integer_ratio()
+print(numerator,denominator, numerator/denominator)
+
+#Chapter 5 : Loops
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+for planet in planets :
+	print(planet, end= ' ') # default end='\n'
+
+multiplicands = (2,2,2,3,3,5)
+product =1
+for mult in multiplicands :
+	product *= mult
+print(product)
+
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+msg = ''
+for char in s :
+	if char.isupper() :
+		print(char, end='')
+
+for i in range(5) :
+	print("i =",i)
+
+i=0
+while i<10 :
+	print(i, end=' ')
+	i+=1
+
+squares = [n**2 for n in range(10)]
+print(squares)
+
+squares = []
+for n in range(10) :
+	squares.append(n**2)
+print(squares)
+
+short_planets = [planet for planet in planets if len(planet) < 6]
+print(short_planets)
+
+loud_short_planets = [planet.upper() + '!' for planet in planets if len(planet) < 6]
+print(loud_short_planets)
+
+print([32 for planet in planets]) # [32, 32, ... , 32]
+
+def count_negatives(nums) :
+	n_negative = 0
+	for num in nums :
+		if num <0 :
+			n_negative++
+	return n_negative
+
+def count_negatives(nums) :
+	return len([num for num in nums if nums<0])
+
+def count_negatives(nums) :
+	return sum([num < 0 for num in nums])
---
+++ kaggleLearn/python3.py
@@ -0,0 +1,74 @@
+#Chapter6 ; Strings and Dictionaries
+x = 'Pluto is a planet'
+y = "Pluto is a planet"
+print(x==y)
+print('Pluto\'s a planet!',"Pluto's a planet!")
+hello = "hello\nworld"
+triplequoted_hello = """hello
+world"""
+print(triplequoted_hello)
+print(hello==triplequoted_hello)
+
+planet = 'Pluto'
+print(planet[0], planet[-3:], len(planet))
+print([char+'!' for char in planet])
+#planet[0] = 'B' # can't modify
+
+claim = "Pluto is a planet!"
+print(claim.upper(),claim.lower())
+print(claim.index('plan')) # 11
+print(claim.startswith(planet)) # Pluto로 시작하는가?
+print(claim.endswith('planet!')) # planet로 끝나는가?
+
+words = claim.split()
+print(words)
+
+datestr = '1956-01-31'
+year, month, day = datestr.split('-')
+print(year, month, day)
+print('/'.join(datestr.split('-')))
+print('/'.join([year, month, day]))
+
+print(' 👏 '.join([word.upper() for word in words]))
+print(planet + ', we miss you')
+
+position = 9
+print(planet + ", you'll always be the " + str(position) + "th planet to me") # int to string 
+
+print("{}, you'll always be the {}th planet to me.".format(planet,position)) # format string. do not need int to string
+
+pluto_mass = 1.303 * 10**22
+earth_mass = 5.9722 * 10**24
+population = 52910390
+print("{} weights about {:.2} kilograms ({:.3%} of Earth's mass). It's home to {:,} Plutonians.".format(planet, pluto_mass, pluto_mass/earth_mass,population))
+
+s="""Pluto's a {0}.
+No, it's a {1}.
+{0}!
+{1}!""".format('planet','dwarf planet')
+print(s)
+
+#Dictionaries 
+numbers = {'one':1, 'two':2, 'three':3}
+print(numbers['one'])
+numbers['eleven'] = 11
+print(numbers)
+numbers['one'] = 'Pluto' # multi type OK!
+print(numbers)
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+planet_to_initial = {planet: planet[0] for planet in planets}
+print(planet_to_initial)
+
+print('Saturn' in planet_to_initial)
+print('Betelgeuse' in planet_to_initial)
+
+for k in numbers :
+	print("{} = {}".format(k, numbers[k]))
+
+print(' '.join(sorted(planet_to_initial.values()))) # dict.keys(), dict.values()
+print(' '.join(sorted(planet_to_initial.keys())))
+
+for planet, initial in planet_to_initial.items() :
+	print("{} = {}".format(planet.rjust(10), initial)) # rjust 우측정렬
+
