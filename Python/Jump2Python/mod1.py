842bb6aa124f0042569fed71ad121f3fde86dcf2
data-droid <blue44rain@gmail.com>
2019-04-24 21:47:02 +0900

    class and module

---
+++ Python/Jump2Python/mod1.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+if __name__ == "__main__":
+    print(add(1,4))
+    print(sub(4,2))
---
+++ Python/Jump2Python/mod2.py
@@ -0,0 +1,7 @@
+
+PI = 3.141592
+class Math:
+    def solv(self, r):
+        return PI * (r**2)
+def add(a,b):
+    return a+b
